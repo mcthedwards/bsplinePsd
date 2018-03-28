86ae385cdfeeb29def71c78fa54beccf6bbda187
mcthedwards <the.hanataua@gmail.com>
2018-03-28 17:18:17 +0100

    Added legend and log to plot.psd

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,6 @@
 ## bsplinePsd 0.5.0
 
-Previous versions only allowed the user to use cubic B-spline densities.  This version allows the user to choose any degree.  Normalising B-splines now uses a trivial integral formula.
+Previous versions only allowed the user to use cubic B-spline densities.  This version allows the user to choose any degree.  Normalising the  B-splines now uses a trivial integral formula.
 
 The function gibbs_bspline can now handle odd length time series.
 
@@ -8,4 +8,4 @@ An S3 plot method has been included so the user can easily plot their PSD estima
 
 ## bsplinePsd 0.2.0
 
-Added an argument called k1 in the gibbs_bspline function.  This allows the user to specify the starting value for parameter k.  If well-chosen, this can speed up convergence significantly.  The default is set to 20, which works well on all of the cases I have come across.  If missing (NA), then a random integer between 5 and kmax will be selected as the starting value for k.
+Added an argument called k1 in the gibbs_bspline function.  This allows the user to specify the starting value for parameter k.  If well-chosen, this can speed up convergence significantly.  The default is set to 20, which works well on all of the cases I have come across.  If missing (NA), then a random integer between degree + 2 and kmax will be selected as the starting value for k.
--- R/internal_gibbs_s3.R
+++ R/internal_gibbs_s3.R
@@ -2,11 +2,15 @@
 #' @description This function plots the log periodogram, log posterior median PSD, and log 90\% credible region PSD.  The x-axis uses angular frequency and the y-axis is plotted on the log scale.  The PSD at the zero frequency is removed from the plot.  If the time series is even length, the PSD at the last frequency is also removed from the plot.
 #' @export
 #' @param x an object of class psd
+#' @param legend.loc location of legend out of "topright" (default), "topleft", "bottomright", "bottomleft"
+#' @param ylog logical value (default is TRUE) to determine if PSD (y-axis) should be on natural log scale
 #' @param ... other graphical parameters from the plot.default function
-#' @return plot of the estimate of the log PSD
+#' @return plot of the estimate of the (log) PSD
 #' @seealso \link{gibbs_bspline}
 #' @examples 
 #' \dontrun{
+#' 
+#' set.seed(12345)
 #' 
 #' # Simulate AR(4) data
 #' n = 2 ^ 7
@@ -15,15 +19,15 @@
 #' data = data - mean(data)
 #' 
 #' # Run MCMC with linear B-spline prior (may take some time)
-#' mcmc = gibbs_bspline(data, 10000, 5000, 1, degree = 1)
+#' mcmc = gibbs_bspline(data, 10000, 5000, degree = 1)
 #' 
 #' # Plot result
 #' plot(mcmc)
 #' 
-#' # Plot result with title
-#' plot(mcmc, main = "Estimate of log PSD using the linear B-spline prior")
+#' # Plot result on original scale with title
+#' plot(mcmc, ylog = FALSE, main = "Estimate of PSD using the linear B-spline prior")
 #' }
-plot.psd = function(x, ...) {  # Plot method for "psd" class
+plot.psd = function(x, legend.loc = "topright", ylog = TRUE, ...) {  # Plot method for "psd" class
   N = length(x$pdgrm)
   freq = seq(0, pi, length = N)
   # Frequencies to remove from estimate
@@ -33,11 +37,26 @@ plot.psd = function(x, ...) {  # Plot method for "psd" class
   else {  # Even length time series
     bFreq = c(1, N)
   }
-  graphics::plot.default(freq[-bFreq], log(x$pdgrm[-bFreq]), type = "l", col = "grey",
-                         xlab = "Frequency", ylab = "log PSD", ...)
-  graphics::lines(freq[-bFreq], log(x$psd.median[-bFreq]), lwd = 2)
-  graphics::lines(freq[-bFreq], log(x$psd.p05[-bFreq]), lwd = 2, lty = 2, col = 4)
-  graphics::lines(freq[-bFreq], log(x$psd.p95[-bFreq]), lwd = 2, lty = 2, col = 4)
-  graphics::legend("topright", legend = c("periodogram", "posterior median", "90% credible region"), 
-                   col = c("grey", "black", "blue"), lwd = c(1, 2, 2), lty = c(1, 1, 2))
+  
+  if (!is.logical(ylog)) stop("ylog must be TRUE or FALSE")
+  
+  if (ylog == TRUE) {
+    graphics::plot.default(freq[-bFreq], log(x$pdgrm[-bFreq]), type = "l", col = "grey",
+                           xlab = "Frequency", ylab = "log PSD", ...)
+    graphics::lines(freq[-bFreq], log(x$psd.median[-bFreq]), lwd = 2)
+    graphics::lines(freq[-bFreq], log(x$psd.p05[-bFreq]), lwd = 2, lty = 2, col = 4)
+    graphics::lines(freq[-bFreq], log(x$psd.p95[-bFreq]), lwd = 2, lty = 2, col = 4)
+    graphics::legend(legend.loc, legend = c("periodogram", "posterior median", "90% credible region"), 
+                     col = c("grey", "black", "blue"), lwd = c(1, 2, 2), lty = c(1, 1, 2))
+  }
+  if (ylog == FALSE) {
+    graphics::plot.default(freq[-bFreq], x$pdgrm[-bFreq], type = "l", col = "grey",
+                           xlab = "Frequency", ylab = "PSD", ...)
+    graphics::lines(freq[-bFreq], x$psd.median[-bFreq], lwd = 2)
+    graphics::lines(freq[-bFreq], x$psd.p05[-bFreq], lwd = 2, lty = 2, col = 4)
+    graphics::lines(freq[-bFreq], x$psd.p95[-bFreq], lwd = 2, lty = 2, col = 4)
+    graphics::legend(legend.loc, legend = c("periodogram", "posterior median", "90% credible region"), 
+                     col = c("grey", "black", "blue"), lwd = c(1, 2, 2), lty = c(1, 1, 2))
+  }
+  
 }
